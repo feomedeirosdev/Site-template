60a3f0428de321515b41b320298eee5f39daf161
Fabio Medeiros <120518007+feomedeirosdev@users.noreply.github.com>
2022-12-30 15:26:10 +0000

    Começo da Landing Page

---
+++ js/script.js
@@ -0,0 +1,23 @@
+function mostrarpdp() {
+	if (pdp.style.display == 'block') {
+		pdp.style.display = 'none';
+	} else {
+		pdp.style.display = 'block';
+	}	
+}
+
+function mostrarted() {
+	if (ted.style.display == 'block') {
+		ted.style.display = 'none';
+	} else {
+		ted.style.display = 'block';
+	}
+}
+
+function mostrartdu() {
+	if (tdu.style.display == 'block') {
+		tdu.style.display = 'none';
+	} else {
+		tdu.style.display = 'block';
+	}
+}
